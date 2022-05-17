ec80835de47df7ebacd9d4a09f91e968779e10a6
semetei <semeteisaidinov@gmail.com>
2022-05-17 19:25:35 +0300

    all

---
+++ apps.js
@@ -0,0 +1,45 @@
+// let mirazim = {
+//     from:'Talas',
+//     name: 'Mirazim',
+//     age:21,
+//     hobbies:{
+//         play:"футбол",
+//         hob:"отдыхать"
+//     }
+ 
+// }
+// // console.log(Object.keys(mirazim))
+// console.log(Object.values(mirazim))
+
+// function num(a,b,c,d){
+//     console.log(a+b/c-d)
+// }
+// num(25,58,69,40)
+// let num =[15,20,80,60,78]
+
+// function nam(mirazim){
+//     document.write(mirazim)
+// }
+// nam(num)
+// let button = document.getElementById('btn')
+// button.addEventListener('mouseout',func)
+// function func(){
+//     alert("you clicked me")
+// }
+// let knop = document.querySelector('.love')
+// knop.addEventListener('click' ,fun)
+// let p = document.getElementById('text')
+
+// function fun(){
+//     p.style.fontSize="60px"
+//     p.style.color="red" 
+//    p.style.border="bottom 1px sollid #0000"
+    
+// }
+
+let syr=document.querySelector(".surot")
+syr.addEventListener("musemove",fucn)
+
+function fucn(){
+    surot.style.tr
+}
